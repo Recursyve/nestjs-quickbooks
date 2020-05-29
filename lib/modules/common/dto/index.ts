fdd13e8d1d86c54031ef17624ccd78ac18a2e5cd
Gabriel Beaudry <gabriel.bdry@gmail.com>
2020-05-29 15:16:58 -0400

    PR fixes

--- lib/modules/common/dto/index.ts
+++ lib/modules/common/dto/index.ts
@@ -2,4 +2,4 @@ export * from "./email.dto";
 export * from "./phone-number.dto";
 export * from "./physical-address.dto";
 export * from "./ref.dto";
-export * from "./website.model";
+export * from "./website.dto";
