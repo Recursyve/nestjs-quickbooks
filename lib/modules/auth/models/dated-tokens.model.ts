78dd7a2c32ae533134effc4d72bb7375da4b82c0
Gabriel Beaudry <gabriel.bdry@gmail.com>
2020-06-01 21:45:11 -0400

    Remove duplicated properties

--- lib/modules/auth/models/dated-tokens.model.ts
+++ lib/modules/auth/models/dated-tokens.model.ts
@@ -2,9 +2,4 @@ import { TokensModel } from "./tokens.model";
 
 export interface DatedTokensModel extends TokensModel {
     created_at: number;
-    refresh_token: string;
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-    x_refresh_token_expires_in: number;
 }
