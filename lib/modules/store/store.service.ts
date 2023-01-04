0bebed043c7afbb9c0735010022b8666ecd296ef
JulienDuf <julien@recursyve.io>
2023-01-04 16:26:08 -0500

    Fix for strict mode

--- lib/modules/store/store.service.ts
+++ lib/modules/store/store.service.ts
@@ -4,7 +4,7 @@ import { TokensModel } from "../auth/models/tokens.model";
 @Injectable()
 export abstract class QuickBooksStore {
     public abstract registerCompany(realm: string): Promise<void>;
-    public abstract getDefaultCompany(): Promise<string>;
+    public abstract getDefaultCompany(): Promise<string | null>;
     public abstract setToken(realm: string, token: TokensModel): Promise<void>;
-    public abstract getToken(realm: string): Promise<TokensModel>;
+    public abstract getToken(realm: string): Promise<TokensModel | null>;
 }
