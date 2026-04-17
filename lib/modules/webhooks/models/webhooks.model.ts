eacc5c2ca1b956e6a554a6208465f8f422844fa5
Gabriel Roberge <groberge@recursyve.io>
2026-04-17 16:32:09 -0400

    refactor(webhooks): convert QuickbooksCloudEventEntity and QuickbooksCloudEventAction to const arrays for improved type safety

--- lib/modules/webhooks/models/webhooks.model.ts
+++ lib/modules/webhooks/models/webhooks.model.ts
@@ -1,41 +1,38 @@
-export type QuickbooksCloudEventEntity =
-    | "account"
-    | "bill"
-    | "billpayment"
-    | "budget"
-    | "class"
-    | "creditmemo"
-    | "currency"
-    | "customer"
-    | "department"
-    | "deposit"
-    | "employee"
-    | "estimate"
-    | "invoice"
-    | "item"
-    | "journalcode"
-    | "journalentry"
-    | "payment"
-    | "paymentmethod"
-    | "preferences"
-    | "purchase"
-    | "purchaseorder"
-    | "refundreceipt"
-    | "salesreceipt"
-    | "taxagency"
-    | "term"
-    | "timeactivity"
-    | "transfer"
-    | "vendor"
-    | "vendorcredit";
+export const quickbooksCloudEventEntities = [
+    "account",
+    "bill",
+    "billpayment",
+    "budget",
+    "class",
+    "creditmemo",
+    "currency",
+    "customer",
+    "department",
+    "deposit",
+    "employee",
+    "estimate",
+    "invoice",
+    "item",
+    "journalcode",
+    "journalentry",
+    "payment",
+    "paymentmethod",
+    "preferences",
+    "purchase",
+    "purchaseorder",
+    "refundreceipt",
+    "salesreceipt",
+    "taxagency",
+    "term",
+    "timeactivity",
+    "transfer",
+    "vendor",
+    "vendorcredit"
+] as const;
+export type QuickbooksCloudEventEntity = (typeof quickbooksCloudEventEntities)[number];
 
-export type QuickbooksCloudEventAction =
-    | "created"
-    | "updated"
-    | "deleted"
-    | "merged"
-    | "voided"
-    | "emailed";
+export const quickbooksCloudEventActions = ["created", "updated", "deleted", "merged", "voided", "emailed"] as const;
+export type QuickbooksCloudEventAction = (typeof quickbooksCloudEventActions)[number];
 
 export type QuickbooksCloudEventType =
     | `qbo.${QuickbooksCloudEventEntity}.${QuickbooksCloudEventAction}.v1`
